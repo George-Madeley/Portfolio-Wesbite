95bc2423ec0889c68fedecf55dd98e1085b065e6
George Madeley <george.madeley@outlook.com>
2023-12-19 12:47:58 +0000

    feat: created work in progress page

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import error from './error.svg';
+import { ReactComponent as Linkedin } from './linkedin.svg';
+import { ReactComponent as Github } from './github.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={error} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Sorry, George Madeley's Portfolio website is still being developed. <br />
+          Please check back later for updates.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Please check out my LinkedIn and GitHub pages for more information.
+        </p>
+        <div className="App-links">
+          <a
+            className="App-link"
+            href="https://www.linkedin.com/in/georgemadeleybathcompsyseng"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Linkedin className='link-icon'/>
+            <p>LinkedIn</p>
+          </a>
+          <a
+            className="App-link"
+            href="https://github.com/George-Madeley"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Github className='link-icon'/>
+            <p>GitHub</p>
+          </a>
+        </div>
       </header>
     </div>
   );

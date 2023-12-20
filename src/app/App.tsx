09aeca02e9192076a17c7ae857632be3dc9fb368
George Madeley <george.madeley@outlook.com>
2023-12-20 20:21:48 +0000

    Added Nav Bar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import Nav from '../components/nav/Nav';
 import Home from "../pages/home/Home";
 
 function App() {
   return (
     <div className="app">
+      <Nav />
       <Home />
     </div>
   );
---
+++ src/components/nav/Nav.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './Nav.css';
+
+export default function Nav() {
+  return (
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Projects</li>
+            <li>Employment</li>
+            <li>Education</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+  )
+}

20fb3c494220b792ca137cc228371521975a4f98
George Madeley <george.madeley@outlook.com>
2023-12-27 16:43:35 +0000

    Improved Nav Bar Responsiveneess

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,15 +1,29 @@
 import React from "react";
 import './Nav.css';
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faUser, faFileCode, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
 export default function Nav() {
   return (
     <nav>
         <ul>
-            <li>Home</li>
-            <li>Projects</li>
-            <li>Employment</li>
-            <li>Education</li>
-            <li>Contact</li>
+            <li>
+              <FontAwesomeIcon icon={faHome} className="nav-icon"/>
+              <p className="nav-text">Home</p>
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faFileCode} className="nav-icon"/>
+              <p className="nav-text">Projects</p>
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faUser} className="nav-icon"/>
+              <p className="nav-text">About Me</p>
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faEnvelope} className="nav-icon"/>
+              <p className="nav-text">Contact</p>
+            </li>
         </ul>
     </nav>
   )

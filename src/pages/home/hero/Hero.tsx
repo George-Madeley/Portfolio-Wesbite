6bf7f4c56af59c4907c6c922aaca6d3221f0f9b5
George Madeley <george.madeley@outlook.com>
2023-12-28 22:27:41 +0000

    refactor: moved gradient to global background

--- src/pages/home/hero/Hero.tsx
+++ src/pages/home/hero/Hero.tsx
@@ -4,7 +4,7 @@ import './Hero.css'
 import portrait from '../../../img/hero.png';
 
 import Socials from './socials/Socials'
-import Heading from './heading/Heading';
+import Heading from '../../../components/heading/Heading';
 import Bio from './bio/Bio';
 
 export default function Hero() {
@@ -12,7 +12,10 @@ export default function Hero() {
   return (
     <header className='hero'>
       <div className='hero-container'>
-        <Heading />
+        <Heading>
+          <p>The Portolio of</p>
+          <p>George Madeley</p>
+        </Heading>
         <div className='hero-image-container'>
           <img className='hero-image' src={portrait} alt='George Madeley' />
         </div>

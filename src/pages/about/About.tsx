06ee29885ff5ef25215c4fc191758f008cf52cea
George Madeley <george.madeley@outlook.com>
2023-12-31 12:56:40 +0000

    feat: added heading to about page

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './About.css'
 
+import Heading from '../../components/heading/Heading'
 import Timeline from './timeline/Timeline'
 
 export default function About() {
   return (
     <div className='about-page'>
+      <Heading>
+        <h1>About Me</h1>
+      </Heading>
       <Timeline />
     </div>
   )

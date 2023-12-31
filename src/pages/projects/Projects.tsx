ae54bb7cb44293112e65515f02d4d4fa58ac597b
George Madeley <george.madeley@outlook.com>
2023-12-31 22:46:07 +0000

    Added Projects Heading

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Projects.css'
 
+import Heading from '../../components/heading/Heading'
+
 export default function Projects() {
   return (
     <div className='projects-page'>
-      Projects
+      <Heading>
+        <h1>Projects</h1>
+      </Heading>
     </div>
   )
 }

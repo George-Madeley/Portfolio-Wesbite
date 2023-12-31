4de1cc9fbc5ec794be132d69255895dcc78a3256
George Madeley <george.madeley@outlook.com>
2023-12-31 19:19:36 +0000

    refactor: encapsulated description styles

--- src/pages/about/description/Description.tsx
+++ src/pages/about/description/Description.tsx
@@ -3,7 +3,7 @@ import './Description.css'
 
 export default function Description() {
   return (
-    <div className='description'>
+    <div className='about-description'>
         <p>
             Amet sit non aliqua nisi laboris ad elit proident dolore. Dolor id ad exercitation sit nostrud ea sint elit. Laboris amet velit mollit mollit occaecat excepteur id id irure ad non sit amet magna. Ex excepteur ad duis ea consectetur. Id cupidatat consectetur nulla tempor aliqua nostrud laboris labore exercitation eiusmod. Cillum officia dolor nisi elit id elit cillum eiusmod proident sint esse Lorem. Magna esse do enim ipsum exercitation ut nisi anim.
         </p>

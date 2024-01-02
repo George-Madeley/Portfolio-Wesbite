47d0033274112e660a675f5b936103ff1a7ec11b
George Madeley <george.madeley@outlook.com>
2024-01-02 14:44:52 +0000

    Abstracted the Timeline Card

---
+++ src/pages/about_page/timeline/card/heading/Heading.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Heading.css'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+
+export default function Heading(props: any) {
+  return (
+    <div className='card-heading'>
+        <h2>{props.position}</h2>
+        {
+            props.companyLink ? (
+                <a className='company-name' href={props.companyLink}>
+                    <h3>{props.company}</h3>
+                    <FontAwesomeIcon icon={faArrowRight} />
+                </a>
+            ) : (
+                <div className='company-name'>
+                    <h3>{props.company}</h3>
+                </div>
+            )
+        }
+    </div>
+  )
+}

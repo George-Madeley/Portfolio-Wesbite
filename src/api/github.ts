554781d8a46f458f613d38bf7d596c0b4b588c0b
George Madeley <george.madeley@outlook.com>
2024-01-02 20:38:40 +0000

    Added Support for GitHub API

---
+++ src/api/github.ts
@@ -0,0 +1,69 @@
+import { Octokit } from 'octokit';
+
+const octokit = new Octokit({ 
+    auth: process.env.REACT_APP_GITHUB_TOKEN
+});
+
+export const getRepos = async (): Promise<any[]> => {
+    try {
+        const response = await octokit.request('GET /users/{username}/repos', {
+            username: 'George-Madeley',
+            headers: {
+                'X-GitHub-Api-Version': '2022-11-28'
+            },
+            type: 'all',
+            sort: 'updated'
+        });
+
+        if (response.status > 299) {
+            throw new Error(`Failed to fetch repos. Status: ${response.status}`);
+        }
+
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
+export const getLanguages = async (owner: string, repo: string): Promise<any> => {
+    try {
+        const response = await octokit.request('GET /repos/{owner}/{repo}/languages', {
+            owner: owner,
+            repo: repo,
+            headers: {
+                'X-GitHub-Api-Version': '2022-11-28'
+            }
+        });
+
+        if (response.status > 299) {
+            throw new Error(`Failed to fetch languages. Status: ${response.status}`);
+        }
+
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+};
+
+export const getNumberOfCommits = async (owner: string, repo: string): Promise<any> => {
+    try {
+        const response = await octokit.request('GET /repos/{owner}/{repo}/stats/contributors', {
+            owner: owner,
+            repo: repo,
+            headers: {
+                'X-GitHub-Api-Version': '2022-11-28'
+            }
+        });
+
+        if (response.status > 299) {
+            throw new Error(`Failed to fetch number of commits. Status: ${response.status}`);
+        }
+
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+};

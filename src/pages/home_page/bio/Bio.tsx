b77344d42c2edd38d28c0bf55d4aaadb15c5d471
George Madeley <george.madeley@outlook.com>
2024-01-02 23:47:53 +0000

    feat: moved text to json

--- src/pages/home_page/bio/Bio.tsx
+++ src/pages/home_page/bio/Bio.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Bio.css';
+
+import json from '../../../data/home_page.real.json';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 export default function Bio() {
+  const [bio, setBio] = React.useState<any[]>([])
+
+  useEffect(() => {
+    setBio(json.bio)
+  }, [bio])
+
   return (
     <div className="bio">
       <div className='bio-bg'>
@@ -13,19 +21,13 @@ export default function Bio() {
 
       <span className="bio-info">
         <div className='bio-text'>
-          <p>Hi, I'm <strong>George</strong>.</p>
-          <p>
-            I'm a student studying an integrated Master's of Engineering in
-            Computer Systems Engineering at the University of Bath. (Aug 2022)
-          </p>
-          <p>
-            With interests in full-stack development and artificial
-            intelligence.
-          </p>
-          <p>
-            You can find me optimizing my code for many projects, building and
-            repairing computers, or even playing video games during my free time.
-          </p>
+          {
+            bio.map((paragraph: any, index: number) => {
+              return (
+                <p key={index}>{paragraph}</p>
+              )
+            })
+          }
         </div>
       </span>
     </div>

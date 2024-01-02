a9d327678cd5d5d8e066e64d59174cf6bff17620
George Madeley <george.madeley@outlook.com>
2024-01-02 20:38:40 +0000

    feat: added support for github api

--- src/pages/projects_page/projects/Projects.tsx
+++ src/pages/projects_page/projects/Projects.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Projects.css'
+
+import { getRepos, getLanguages, getNumberOfCommits } from '../../../api/github'
 
 import Tile from './tile/Tile'
 
 export default function Projects() {
   const [selectedTile, setSelectedTile] = React.useState('' as string)
+
+  const [repos, setRepos] = React.useState([] as any[]);
 
   const handleTileSelect = (event: any) => {
     if (event.target.checked) {
@@ -14,6 +18,34 @@ export default function Projects() {
     setSelectedTile('')
   }
 
+  useEffect(() => {
+    getRepos().then((repos) => {
+      Promise.all(
+        repos.map((repo: any) => {
+          return Promise.all([
+            getLanguages(repo.owner.login, repo.name),
+            getNumberOfCommits(repo.owner.login, repo.name)
+          ]).then(([languages, numberOfCommits]) => {
+            const newRepo = {
+              "id": repo.id,
+              "name": repo.name,
+              "description": repo.description,
+              "html_url": repo.html_url,
+              "stargazers_count": repo.stargazers_count,
+              "updated_at": repo.updated_at.substring(0, 4),
+              "languages": Object.keys(languages),
+              "commits_count": numberOfCommits[0].total
+            }
+            return newRepo;
+          });
+        })
+      ).then((reposWithDetails) => {
+        console.log(reposWithDetails);
+        setRepos(reposWithDetails);
+      });
+    });
+  }, [])
+
   return (
     <div className='projects'>
       <div className='table-header'>
@@ -22,41 +54,32 @@ export default function Projects() {
         <p className='languages'>Languages</p>
         <p className='link'>Link</p>
       </div>
-      <Tile
-        id='projects-1'
-        date='2021'
-        name='Portfolio'
-        languages={['React', 'TypeScript', 'CSS', 'HTML']}
-        link=''
-        linkText='portfolio.com'
-        stars='0'
-        commits='0'
-        isSelected={selectedTile === 'projects-1'}
-        handleChange={handleTileSelect}
-      >
-        <p>
-          Culpa laborum culpa cillum cillum incididunt exercitation id deserunt esse consectetur est reprehenderit. Mollit commodo esse adipisicing nostrud exercitation nostrud fugiat. Cillum cillum excepteur sunt quis laboris tempor dolore Lorem aute consequat. Ullamco ex magna voluptate adipisicing. Eu ex dolor non sit elit consequat nisi in. Mollit elit incididunt veniam aliqua cillum excepteur consectetur. Est nisi eu mollit minim culpa qui.
-
-          Consequat aliqua cillum amet tempor et voluptate. Minim officia ex qui excepteur reprehenderit pariatur sint ea aute qui non. Amet exercitation sit nostrud laborum fugiat sint Lorem aliqua deserunt excepteur ad. Et pariatur nostrud eu sint in et sunt aliquip ullamco culpa irure et nostrud. Voluptate pariatur veniam culpa ut anim sint ut cillum occaecat anim amet dolore.
-        </p>
-      </Tile>
-      <Tile
-        id='projects-2'
-        date='2021'
-        name='Portfolio'
-        languages={['React', 'TypeScript', 'CSS', 'HTML', 'JavaScript']}
-        link=''
-        linkText='portfolio.com'
-        stars='0'
-        commits='0'
-        isSelected={selectedTile === 'projects-2'}
-        handleChange={handleTileSelect}
-      >
-        <p>
-          This is the portfolio you are currently viewing. It was created using React, TypeScript, CSS, and HTML. It is
-          hosted on GitHub pages.
-        </p>
-      </Tile>
+      {
+        repos.map((repo: any, index: number) => {
+          return (
+            <Tile
+              key={index}
+              id={repo.id}
+              date={repo.updated_at}
+              name={repo.name.replace(/-/g, ' ')}
+              languages={repo.languages}
+              link={repo.html_url}
+              linkText={`${repo.name}.git`}
+              stars={repo.stargazers_count}
+              commits={repo.commits_count}
+              isSelected={selectedTile === `${repo.id}`}
+              handleChange={handleTileSelect}
+            >
+              <h4>
+                Description
+              </h4>
+              <p>
+                {repo.description}
+              </p>
+            </Tile>
+          )
+        })
+      }
     </div>
   )
 }

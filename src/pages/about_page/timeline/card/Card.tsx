a8a78941a14a324d17419c95d64f40f648f55672
George Madeley <george.madeley@outlook.com>
2024-01-02 14:44:52 +0000

    refactor: abstracted the timeline card

--- src/pages/about_page/timeline/card/Card.tsx
+++ src/pages/about_page/timeline/card/Card.tsx
@@ -1,26 +1,11 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React from 'react'
 import './Card.css'
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLink, faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import Heading from './heading/Heading'
+import Content from './content/Content'
+import Footer from './footer/Footer'
 
 export default function Card(props: any) {
-    const [isReadMoreVisible, setReadMoreVisible] = useState(false);
-
-    const contentTextRef = useRef<HTMLDivElement>(null);
-    const contentTextInnerRef = useRef<HTMLDivElement>(null);
-
-    useEffect(() => {
-        if (contentTextRef.current) {
-            const contentHeight = contentTextRef.current.offsetHeight;
-            const contentInnerHeight = contentTextInnerRef.current?.offsetHeight;
-            if (contentInnerHeight && contentHeight < contentInnerHeight) {
-                setReadMoreVisible(true);
-            }
-        }
-    }, []);
-
-
     return (
         <div className='timeline-card-container'>
             <div className='timeline-card'>
@@ -28,56 +13,11 @@ export default function Card(props: any) {
                     <p>{props.time}</p>
                 </div>
                 <div className='content'>
-                    <h2>{props.position}</h2>
-                    {
-                        props.companyLink ? (
-                            <a className='company-name' href={props.companyLink}>
-                                <h3>{props.company}</h3>
-                                <FontAwesomeIcon icon={faArrowRight} />
-                            </a>
-                        ) : (
-                            <div className='company-name'>
-                                <h3>{props.company}</h3>
-                            </div>
-                        )
-                    }
-                    <input type='checkbox' id={`read-more-${props.id}`} name='read-more' />
-                    <div className='content-text' ref={contentTextRef}>
-                        <div className='content-text-inner' ref={contentTextInnerRef}>
-                            {
-                                props.children
-                            }
-                        </div>
-                    </div>
-                    <div className='label-container' style={{display: isReadMoreVisible ? 'block' : 'none'}}>
-                        <label htmlFor={`read-more-${props.id}`}>
-                            <span>Read More</span>
-                            <span>Read Less</span>
-                        </label>
-                    </div>
-                    <ul className='project-links'>
-                        {
-                            props.links.map((link: any, index: number) => {
-                                return (
-                                    <li key={index}>
-                                        <a href={link.url} target='_blank' rel='noopener noreferrer'>
-                                            <FontAwesomeIcon icon={faLink} />
-                                            <p>{link.name}</p>
-                                        </a>
-                                    </li>
-                                )
-                            })
-                        }
-                    </ul>
-                    <ul className='language-list'>
-                        {
-                            props.languages.map((language: any, index: number) => {
-                                return (
-                                    <li key={index}>{language}</li>
-                                )
-                            })
-                        }
-                    </ul>
+                    <Heading position={props.position} company={props.company} companyLink={props.companyLink} />
+                    <Content id={props.id}>
+                        {props.children}
+                    </Content>
+                    <Footer links={props.links} languages={props.languages} />
                 </div>
             </div>
         </div>

2d5eddfd035b6ff3bf9075e4f3c4fa37e95caec4
George Madeley <george.madeley@outlook.com>
2024-01-05 21:32:29 +0000

    Changed to use h5

--- src/pages/home_page/hero/bio/Bio.tsx
+++ src/pages/home_page/hero/bio/Bio.tsx
@@ -4,13 +4,13 @@ import './Bio.css'
 export default function Bio() {
   return (
     <div className='hero-bio'>
-        <p className='hero-bio-text'>
+        <h5 className='hero-bio-text'>
             Masters of Computer Systems Engineering student at the University of
             Bath,
-        </p>
-        <p className='hero-bio-text'>
+        </h5>
+        <h5 className='hero-bio-text'>
             Full Stack Developer | Machine Learning Engineer | Game Developer
-        </p>
+        </h5>
     </div>
   )
 }

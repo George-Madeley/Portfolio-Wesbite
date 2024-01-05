1fb2550f9250f008c6d86f2a6d5dd62a2d4c0ed7
George Madeley <george.madeley@outlook.com>
2024-01-05 21:32:48 +0000

    fix: moved button position to bottom

--- src/pages/home_page/highlights/tile/Tile.tsx
+++ src/pages/home_page/highlights/tile/Tile.tsx
@@ -8,20 +8,22 @@ import Button from '../../../../components/button/Button';
 export default function Tile(props: any) {
     return (
         <div className='tile'>
-            <img src={thumbail} alt='thumbnail' />
-            <div className='language-container'>
-                {
-                    props.languages.map((language: string, index: number) => {
-                        return (
-                            <div key={index} className='language'>
-                                <p>{language}</p>
-                            </div>
-                        )
-                    })
-                }
+            <div className='content-container'>
+                <img src={thumbail} alt='thumbnail' />
+                <div className='language-container'>
+                    {
+                        props.languages.map((language: string, index: number) => {
+                            return (
+                                <div key={index} className='language'>
+                                    <p>{language}</p>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+                <h5 className='title'>{props.name}</h5>
+                <p className='description'>{props.description}</p>
             </div>
-            <p className='title'>{props.name}</p>
-            <p className='description'>{props.description}</p>
             <div className='button-container'>
                 <Button link={props.link} icon='code' text='Learn More' />
             </div>

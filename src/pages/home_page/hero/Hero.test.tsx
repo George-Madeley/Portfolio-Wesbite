ca5247b42df96323f74a2d408dff6a46d0b69756
George Madeley <george.madeley@outlook.com>
2024-01-06 17:12:34 +0000

    Removed Tests

--- src/pages/home_page/hero/Hero.test.tsx
+++ src/pages/home_page/hero/Hero.test.tsx
@@ -9,19 +9,4 @@ describe("Hero", () => {
     it('renders without crashing', () => {
         render(<Hero />);
     });
-
-
-    test("Check for Portfolio in Title", () => {
-        const expectedValue = "The Portfolio of";
-        render(<Hero />);
-        const titleElement = screen.getByText(expectedValue);
-        expect(titleElement).toBeInTheDocument();
-    });
-
-    test("Check for Name in Title", () => {
-        const expectedValue = "George Madeley";
-        render(<Hero />);
-        const titleElement = screen.getByText(expectedValue);
-        expect(titleElement).toBeInTheDocument();
-    });
 })

2ccbf73bedea4bff0a1e091291bb650427a851e6
George Madeley <george.madeley@outlook.com>
2024-01-06 14:47:09 +0000

    Aded Error Tiles

---
+++ src/pages/home_page/highlights/error_tile/ErrorTile.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './ErrorTile.css'
+
+import thumbail from '../../../../img/code-thumbnail.png'
+
+export default function ErrorTile(props: any) {
+  return (
+    <div className='error-tile'>
+        <div className='content-container'>
+            <div className='img-container'>
+                <img src={thumbail} alt='thumbnail' />
+            </div>
+            <div className='language-container'></div>
+            <div className='title'>
+                <h5>Error Occured</h5>
+            </div>
+            <div className='description'>
+                <p>{props.message}</p>
+            </div>
+        </div>
+    </div>
+  )
+}

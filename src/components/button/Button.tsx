9f4debc071742407815d1c575f9f6f8fb7e0b92d
George Madeley <george.madeley@outlook.com>
2025-04-20 20:27:45 +0100

    build: switched to next.js

--- src/components/button/Button.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./Button.css";
-
-export default function Button(props: any) {
-  return <div className="button">{props.children}</div>;
-}
--- src/pages/home_page/hero/Hero.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import Hero from "../../../components/heading/Heading";
-
-describe("Hero", () => {
-  it("renders without crashing", () => {
-    render(<Hero />);
-  });
-});

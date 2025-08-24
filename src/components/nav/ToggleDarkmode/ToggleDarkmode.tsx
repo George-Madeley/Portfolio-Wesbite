4fd2d3b0b427c8c69799315dbc6638eabfcaf935
George Madeley <george.madeley@outlook.com>
2025-08-24 17:46:04 +0100

    fix: mui handles dark mode

--- src/components/nav/ToggleDarkmode/ToggleDarkmode.tsx
+++ src/components/nav/ToggleDarkmode/ToggleDarkmode.tsx
@@ -2,23 +2,17 @@
 
 import "./ToggleDarkmode.css";
 
-import React, { useContext } from "react";
-import { ThemeContext } from "~/context";
+import { useCallback } from "react";
+import { useColorScheme } from "@mui/material/styles";
+import Switch from "@mui/material/Switch";
 
 export function ToggleDarkMode() {
-  const { isDarkMode, toggleDarkMode } = useContext(ThemeContext);
+  const { mode, setMode } = useColorScheme();
 
-  return (
-    <div className="toggle-container">
-      <input
-        checked={isDarkMode}
-        id="toggle"
-        name="toggle"
-        onChange={toggleDarkMode}
-        type="checkbox"
-        value={isDarkMode ? "dark" : "light"}
-      />
-      <label htmlFor="toggle" title="dark mode toggle"></label>
-    </div>
-  );
+  const toggleMode = useCallback(() => {
+    console.log(mode);
+    setMode(mode === "dark" ? "light" : "dark");
+  }, [mode, setMode]);
+
+  return <Switch checked={mode === "dark"} onChange={toggleMode} />;
 }

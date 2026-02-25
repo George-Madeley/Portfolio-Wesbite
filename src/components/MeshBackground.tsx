5159b9a0590988e5c778078baf1e45656423e66b
George Madeley <george.madeley@outlook.com>
2026-02-25 23:37:28 +0000

    style: changed backend colour palette

--- src/components/MeshBackground.tsx
+++ src/components/MeshBackground.tsx
@@ -2,31 +2,43 @@
 
 import { PropsWithChildren } from "react";
 import { MeshGradient, MeshGradientProps } from "@mesh-gradient/react";
-import { useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
+import { useColorScheme } from "@mui/material";
 
 export default function MeshBackground(
   props: PropsWithChildren<MeshGradientProps>
 ) {
-  const theme = useTheme();
+  const { colorScheme } = useColorScheme();
 
   return (
     <Box
-      sx={{
-        minHeight: "100dvh",
-        width: "100%",
-        position: "relative",
-      }}
+      sx={[
+        (theme) => ({
+          minHeight: "100dvh",
+          width: "100%",
+          position: "relative",
+          "--mesh-gradient-color-1": "#F7F7F7",
+          "--mesh-gradient-color-2": "#F0F0F0",
+          "--mesh-gradient-color-3": "#E8E8E8",
+          "--mesh-gradient-color-4": "#E0E0E0",
+        }),
+        (theme) =>
+          theme.applyStyles("dark", {
+            minHeight: "100dvh",
+            width: "100%",
+            position: "relative",
+            "--mesh-gradient-color-1": "#121212",
+            "--mesh-gradient-color-2": "#1a1a1a",
+            "--mesh-gradient-color-3": "#212121",
+            "--mesh-gradient-color-4": "#282828",
+          }),
+      ]}
     >
       <MeshGradient
         {...props}
+        key={colorScheme}
         options={{
-          colors: [
-            theme.palette.primary.dark,
-            theme.palette.primary.main,
-            theme.palette.primary.light,
-            theme.palette.primary.main,
-          ],
+          cssVariablesFallback: true,
         }}
         style={{
           position: "fixed",
@@ -35,7 +47,7 @@ export default function MeshBackground(
           top: 0,
           right: 0,
           zIndex: -1,
-          backgroundColor: theme.palette.primary.main,
+          backgroundColor: "var(--mui-palette-background-default)",
           ...props.style,
         }}
       />

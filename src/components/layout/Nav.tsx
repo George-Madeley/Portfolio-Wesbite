67da2e047a83d5509088768091dc81cd8c0858b9
George Madeley <george.madeley@outlook.com>
2026-02-28 13:41:35 +0000

    refactor: changed navigation bar to show selected page

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,13 +1,45 @@
+"use client";
+
 import DataObjectIcon from "@mui/icons-material/DataObject";
 import HomeIcon from "@mui/icons-material/Home";
 import PersonIcon from "@mui/icons-material/Person";
 import Card from "@mui/material/Card";
 import Stack from "@mui/material/Stack";
+import ToggleButton from "@mui/material/ToggleButton";
+import Typography from "@mui/material/Typography";
+import { usePathname } from "next/navigation";
+import { ReactNode } from "react";
 
-import FlexButton from "../FlexButton";
+import StyledToggleButtonGroup from "~/style/StyledToggleButtonGroup";
+
 import ThemeSwitcher from "../ThemeSwitcher";
 
 export default function Nav() {
+  const pathname = usePathname();
+
+  const links: { label: string; href: string; icon: ReactNode }[] = [
+    {
+      label: "Home",
+      href: "/",
+      icon: <HomeIcon />,
+    },
+    {
+      label: "Projects",
+      href: "/projects",
+      icon: <DataObjectIcon />,
+    },
+    {
+      label: "About Me",
+      href: "/about",
+      icon: <PersonIcon />,
+    },
+  ];
+
+  const isActive = (href: string) =>
+    href === "/"
+      ? pathname === "/"
+      : pathname === href || pathname.startsWith(`${href}/`);
+
   return (
     <Stack
       alignItems="center"
@@ -19,43 +51,43 @@ export default function Nav() {
         sx={{
           width: "fit-content",
           m: 1,
-          p: 1,
+          p: 0.5,
           position: "fixed",
           zIndex: 1000,
         }}
       >
-        <Stack
-          alignItems="center"
-          direction="row"
-          gap={{ xs: 1, sm: 3 }}
-          justifyContent="center"
-        >
-          <FlexButton
-            breakCondition="down"
-            breakpoint="sm"
-            href={"/"}
-            startIcon={<HomeIcon />}
-          >
-            Home
-          </FlexButton>
-          <FlexButton
-            breakCondition="down"
-            breakpoint="sm"
-            href={"/projects"}
-            startIcon={<DataObjectIcon />}
-          >
-            Projects
-          </FlexButton>
-          <FlexButton
-            breakCondition="down"
-            breakpoint="sm"
-            href={"/about"}
-            startIcon={<PersonIcon />}
-          >
-            About Me
-          </FlexButton>
+        <StyledToggleButtonGroup>
+          {links.map((link) => (
+            <ToggleButton
+              color="primary"
+              href={link.href}
+              key={link.href}
+              selected={isActive(link.href)}
+              size="small"
+              value={link.label.toLowerCase()}
+            >
+              <Stack
+                alignItems="center"
+                direction="row"
+                gap={1}
+                sx={{ pr: { xs: 0, sm: 1 } }}
+              >
+                {link.icon}
+                <Typography
+                  sx={{
+                    display: {
+                      xs: "none",
+                      sm: "block",
+                    },
+                  }}
+                >
+                  {link.label}
+                </Typography>
+              </Stack>
+            </ToggleButton>
+          ))}
           <ThemeSwitcher />
-        </Stack>
+        </StyledToggleButtonGroup>
       </Card>
     </Stack>
   );

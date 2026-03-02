22fd2219183673ce7a3ded40f0f4e4d4bedddc88
George Madeley <george.madeley@outlook.com>
2026-03-02 21:25:36 +0000

    refactor: updated footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,21 @@
 import "./globals.css";
 
+import EmailIcon from "@mui/icons-material/Email";
+import PhoneIcon from "@mui/icons-material/Phone";
 import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
 import InitColorSchemeScript from "@mui/material/InitColorSchemeScript";
 import { ThemeProvider } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata, Viewport } from "next";
 
+import CopyButton from "~/components/CopyButton";
 import Footer from "~/components/layout/Footer";
 import Nav from "~/components/layout/Nav";
+import SocialLinks from "~/components/SocialLinks";
 import { roboto } from "~/style/font";
 import theme from "~/style/theme";
 
@@ -67,6 +73,14 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const contactInfo = [
+    {
+      icon: <EmailIcon />,
+      text: "george.madeley@outlook.com",
+      id: "email",
+    },
+    { icon: <PhoneIcon />, text: "+44 7830 979199", id: "phone" },
+  ];
   return (
     <html className={roboto.className} lang="en" suppressHydrationWarning>
       <body>
@@ -80,7 +94,69 @@ export default function RootLayout({
             <CssBaseline />
             <Nav />
             <main>{children}</main>
-            <Footer />
+            <Footer
+              sections={[
+                {
+                  title: "Jobs",
+                  gridProps: { size: { xs: 12, sm: 6 } },
+                  items: [
+                    {
+                      title: "Atlantic Technology Ltd",
+                      link: "https://www.atlantictechnology.co.uk/",
+                    },
+                    {
+                      title: "Biodevices Without Borders",
+                      link: "https://bathbiodevices.com/",
+                    },
+                    {
+                      title: "British Telecommunications",
+                      link: "https://www.bt.com/",
+                    },
+                  ],
+                },
+                {
+                  title: "Projects",
+                  gridProps: { size: { xs: 12, sm: 6 } },
+                  items: [
+                    {
+                      title: "EE40140-Magnetic-Induction-Tomography",
+                      link: "/projects/EE40140-Magnetic-Induction-Tomography?owner=George-Madeley",
+                    },
+                    {
+                      title: "GameDevTV-Unity3D-RealmRush",
+                      link: "/projects/GameDevTV-Unity3D-RealmRush?owner=George-Madeley",
+                    },
+                    {
+                      title: "Biodevices App",
+                      link: "/projects/App?owner=Bath-Biodevices-Without-Borders",
+                    },
+                  ],
+                },
+              ]}
+              socials={<SocialLinks />}
+              title="George Madeley"
+              usefulLinks={contactInfo.map((info) => (
+                <Grid alignItems="center" container gap={1} key={info.id}>
+                  <Grid size="auto">
+                    <CopyButton
+                      color="inherit"
+                      label={info.id}
+                      size="small"
+                      text={info.text}
+                    />
+                  </Grid>
+                  <Grid size="grow">
+                    <Typography
+                      color="textSecondary"
+                      noWrap
+                      textOverflow="ellipsis"
+                    >
+                      {info.text}
+                    </Typography>
+                  </Grid>
+                </Grid>
+              ))}
+            />
           </ThemeProvider>
         </AppRouterCacheProvider>
         <Analytics />
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,82 +1,144 @@
-import EmailIcon from "@mui/icons-material/Email";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import PhoneIcon from "@mui/icons-material/Phone";
-import Button from "@mui/material/Button";
-import Card from "@mui/material/Card";
+"use client";
+
 import CardContent from "@mui/material/CardContent";
-import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+import Grid, { GridProps } from "@mui/material/Grid";
+import List from "@mui/material/List";
+import ListSubheader from "@mui/material/ListSubheader";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
+import { motion } from "motion/react";
+import { ReactNode } from "react";
 
-import CopyButton from "../CopyButton";
+import GraphicsCard from "../GraphicsCard";
+import SelectItem, { SelectItemProps } from "../SelectItem";
 
-export default function Footer() {
-  const contactInfo = [
-    {
-      icon: <EmailIcon />,
-      text: "george.madeley@outlook.com",
-      id: "email",
-    },
-    { icon: <PhoneIcon />, text: "+44 7830 979199", id: "phone" },
-  ] as const;
+interface FooterProps {
+  title: string;
+  usefulLinks: ReactNode | ReactNode[];
+  sections: {
+    gridProps?: GridProps;
+    title: string;
+    items: SelectItemProps[];
+  }[];
+  socials: ReactNode | ReactNode[];
+}
 
+export default function Footer({
+  usefulLinks,
+  sections,
+  socials,
+  title,
+}: FooterProps) {
   return (
-    <Stack
-      alignItems="center"
-      component="footer"
-      justifyContent="end"
-      sx={{ width: "100%" }}
-    >
-      <Card sx={{ width: "100%", pt: 7, pb: 6 }}>
-        <CardContent>
-          <Grid
-            alignItems="center"
-            container
-            gap={"20%"}
-            justifyContent="center"
-            sx={{ width: "100%" }}
+    <Container>
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        transition={{
+          duration: 0.5,
+          delay: 0.4,
+        }}
+        viewport={{ once: true }}
+        whileInView={{ opacity: 1, y: 0 }}
+      >
+        <GraphicsCard sx={{ mb: 3 }}>
+          <Stack
+            gap={{ xs: 3, sm: 4, md: 5 }}
+            rel="noopener noreferrer"
+            role="contentinfo"
           >
-            <Grid container direction="column" gap={3}>
-              {contactInfo.map((info) => (
-                <Grid alignItems="center" container gap={1} key={info.id}>
-                  <Grid size="auto">{info.icon}</Grid>
-                  <Grid size="grow">
-                    <Typography>{info.text}</Typography>
-                  </Grid>
-                  <Grid size="auto">
-                    <CopyButton label={info.id} text={info.text} />
+            <CardContent>
+              <Grid container spacing={{ xs: 4, md: 3 }}>
+                <Grid size={{ xs: 12, md: "auto" }}>
+                  <Stack
+                    alignItems={{ sm: "end", md: "start" }}
+                    direction={{ sm: "row", md: "column" }}
+                    gap={3}
+                    justifyContent={{ sm: "start", md: "space-between" }}
+                    sx={{ height: 1 }}
+                  >
+                    <Stack alignItems="flex-start" gap={{ xs: 1.5, sm: 3 }}>
+                      <Typography variant="h3">{title}</Typography>
+                      <Typography
+                        sx={{ maxWidth: { sm: 280 } }}
+                        variant="body2"
+                      >
+                        Explore the different versions of my portfolio
+                      </Typography>
+                    </Stack>
+                    <Stack gap={1}>{usefulLinks}</Stack>
+                  </Stack>
+                </Grid>
+                <Grid size={{ xs: 12, md: "grow" }}>
+                  <Grid
+                    container
+                    spacing={{ xs: 2.5, md: 4 }}
+                    sx={{ justifyContent: "space-between" }}
+                  >
+                    {sections.map((section, sectionIndex) => (
+                      <Grid key={sectionIndex} {...section.gridProps}>
+                        <List
+                          component="nav"
+                          sx={{
+                            p: 1,
+                            width: "100%",
+                            maxWidth: 1,
+                            display: "flex",
+                            flexDirection: "column",
+                          }}
+                          {...(section.title && {
+                            subheader: (
+                              <ListSubheader
+                                sx={{
+                                  p: 1,
+                                  color: "text.primary",
+                                  bgcolor: "transparent",
+                                }}
+                              >
+                                <Typography variant="h6">
+                                  {section.title}
+                                </Typography>
+                              </ListSubheader>
+                            ),
+                          })}
+                        >
+                          {section.items.map((item, index) => (
+                            <SelectItem key={index} {...item} />
+                          ))}
+                        </List>
+                      </Grid>
+                    ))}
                   </Grid>
                 </Grid>
-              ))}
-            </Grid>
-            <Grid container direction="column" gap={3}>
-              <Grid>
-                <Button
-                  fullWidth
-                  href="https://github.com/George-Madeley"
-                  rel="noreferrer"
-                  startIcon={<GitHubIcon />}
-                  target="_blank"
-                >
-                  Github
-                </Button>
               </Grid>
-              <Grid>
-                <Button
-                  fullWidth
-                  href="https://www.linkedin.com/in/georgemadeleybathcompsyseng"
-                  rel="noreferrer"
-                  startIcon={<LinkedInIcon />}
-                  target="_blank"
+            </CardContent>
+            <GraphicsCard overlay>
+              <Stack
+                alignItems="center"
+                direction={{ sm: "row" }}
+                gap={1.5}
+                justifyContent={{ xs: "center", sm: "space-between" }}
+                sx={{
+                  py: { xs: 2, sm: 1.5 },
+                  px: { xs: 2, sm: 3 },
+                }}
+              >
+                <Stack
+                  alignItems={{ xs: "center", md: "flex-start" }}
+                  gap={2}
+                  sx={{
+                    textAlign: { xs: "center", md: "left" },
+                  }}
                 >
-                  LinkedIn
-                </Button>
-              </Grid>
-            </Grid>
-          </Grid>
-        </CardContent>
-      </Card>
-    </Stack>
+                  <Stack direction="row" gap={{ xs: 0.5, sm: 1.5 }}>
+                    {socials}
+                  </Stack>
+                </Stack>
+              </Stack>
+            </GraphicsCard>
+          </Stack>
+        </GraphicsCard>
+      </motion.div>
+    </Container>
   );
 }
